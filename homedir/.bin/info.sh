6afd7e674fdc53cd8c3fa83026ea5a13897a768e
joris <joris@jorisvandijk.com>
2021-10-27 22:43:32 +0200

    Changing the i3 config again

---
+++ homedir/.bin/info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Usage: Notify-send current system info, called from the
+#        i3 config file by bindsym.
+#
+# Script by Joris van Dijk | gitlab.com/jorisvandijk 
+#
+#          Published under GPL-3.0-or-later
+
+notify-send -u critical -t 15000 "$(
+    echo $(date +"%A %-d %B %Y %R")
+    echo
+    bat=$(acpi | awk {'print $3'})
+    if [[ $bat == Charging, ]]; then
+        echo $(echo "Battery at"; acpi | awk '{print substr($4, 1, length($4)-1)}'; echo "and charging")
+    else
+        echo $(echo "Battery at"; acpi | awk '{print substr($4, 1, length($4)-1)}')
+    fi
+)"
