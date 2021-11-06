54425791d22378e1169ce08947861a8b91d90356
joris <joris@jorisvandijk.com>
2021-11-06 17:04:45 +0100

    new editor and cleaning

--- homedir/.bin/rofi_config_menu.sh
+++ homedir/.bin/rofi_config_menu.sh
@@ -9,7 +9,7 @@
 # Defining the text editor to use.
 # DMENUEDITOR="vim"
 # DMENUEDITOR="nvim"
-DMEDITOR="kitty -e micro"
+DMEDITOR="code"
 
 # An array of options to choose.
 # You can edit this list to add/remove config files.
