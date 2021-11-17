e50fdc5cdaf0605da6fab8fe1a4fc371de930707
joris <joris@jorisvandijk.com>
2021-11-17 23:58:22 +0100

    Dang it works

--- homedir/.bin/i3_app.sh
+++ homedir/.bin/i3_app.sh
@@ -1,12 +1,58 @@
 #!/bin/bash
 
+if [ $1 == "firefox" ]; then
+  set -- "GeckoMain"
+fi
+
+if [ $1 == "libreoffice" ]; then
+  set -- "soffice.bin"
+fi
+
+# if program is already running...
 if pgrep -x "$1" > /dev/null; then
-	echo "Running"
+	
+  if [ $1 == "GeckoMain" ]; then
+  set -- "firefox"
+  fi
+  	
+  if [ $1 == "soffice.bin" ]; then
+  set -- "libreoffice"
+  fi
+
   i3-msg '[class=(?i)'$1'] focus' 
+  
+# or if program isn't running...  
 else
-	echo "Not running"
+	
+  if [ $1 == "GeckoMain" ]; then
+  set -- "firefox"
+  fi
+     
+  if [ $1 == "soffice.bin" ]; then
+  set -- "libreoffice"
+  fi
+
   i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1)) && $1 &
 fi
 
-# firefox doesn't work
+
 # libreoffice soffice.bin
+
+
+# if pgrep -x "GeckoMain" > /dev/null; then
+# 	echo "Running"
+  
+# else
+# 	echo "Not running"
+  
+# fi
+# echo $1
+
+  # check=$(ps aux | grep -i "GeckoMain" | wc -l)
+  # if [[ $check -eq "1" ]] ; then
+  #   echo "not running"
+  #   exit;
+  # else
+  #   echo "running"
+  #   exit;
+  # fi
