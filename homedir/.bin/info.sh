88ace53af7292eb06a3d4c09e2ef055d41b6b474
joris <joris@jorisvandijk.com>
2021-12-04 16:58:43 +0100

    Simplifying with icons an no fluff text

--- homedir/.bin/info.sh
+++ homedir/.bin/info.sh
@@ -7,15 +7,28 @@
 #
 #          Published under GPL-3.0-or-later
 
+# notify-send -u normal -t 15000 "$(
+#     printf "The time is $(date +"%R") on\n"
+#     printf "$(date +"%A %-d %B %Y")\n"
+#     bat=$(acpi | awk {'print $3'})
+#     if [[ $bat == Charging, ]]; then
+#         echo $(echo "Battery is at"; acpi | awk '{print substr($4, 1, length($4)-1)}'; echo "and charging")
+#     else
+#         echo $(echo "Battery is at"; acpi | awk '{print substr($4, 1, length($4)-1)}')
+#     fi
+    
+#     printf "You're on workspace $(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)"
+# )"  
+
 notify-send -u normal -t 15000 "$(
-    printf "The time is $(date +"%R") on\n"
-    printf "$(date +"%A %-d %B %Y")\n"
+    printf " $(date "+%A %-d %-B %Y")\n"
+    printf " $(date "+%H:%M")\n"
     bat=$(acpi | awk {'print $3'})
     if [[ $bat == Charging, ]]; then
-        echo $(echo "Battery is at"; acpi | awk '{print substr($4, 1, length($4)-1)}'; echo "and charging")
+        echo $(echo "" ; acpi | awk '{print substr($4, 1, length($4)-1)}')
     else
-        echo $(echo "Battery is at"; acpi | awk '{print substr($4, 1, length($4)-1)}')
+        echo $(echo "" ; acpi | awk '{print substr($4, 1, length($4)-1)}')
     fi
     
-    printf "You're on workspace $(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)"
+    printf " $(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)"
 )"
