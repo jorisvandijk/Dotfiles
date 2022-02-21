7d56b9e5067fb81da34aab08a4d3975b224f300d
joris <joris@jorisvandijk.com>
2022-02-21 10:02:21 +0100

    moved screenshot directory

--- bin/bin/ScreenShot.sh
+++ bin/bin/ScreenShot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scrot ~/pictures/%Y-%m-%d-%T-screenshot.png -e 'xclip -selection clipboard -t image/png -i $f'
+scrot ~/pictures/ss/%Y-%m-%d-%T-screenshot.png -e 'xclip -selection clipboard -t image/png -i $f'
