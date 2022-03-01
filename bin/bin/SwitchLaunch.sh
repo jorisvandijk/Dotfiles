a71e15e7e01d3eae01d66c7de4f21b340ec27b66
joris <joris@jorisvandijk.com>
2022-03-01 09:40:30 +0100

    I dunno

--- bin/bin/SwitchLaunch.sh
+++ bin/bin/SwitchLaunch.sh
@@ -4,7 +4,7 @@
 i3-msg workspace "${1^}"
 
 if [ $1 == newsboat ]; then
-    wmctrl -x -a "$1" || kitty --hold --detach --class newsboat -e newsboat
+    wmctrl -x -a "mpv" || wmctrl -x -a "$1" || kitty --hold --detach --class newsboat -e newsboat
 else
     wmctrl -x -a "$1" || $1
 fi
