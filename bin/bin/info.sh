b2c3db2f8e3e77c7e4d989da396e6dc186e2ba61
joris <joris@jorisvandijk.com>
2022-03-03 20:26:00 +0100

    Several

--- bin/bin/info.sh
+++ bin/bin/info.sh
@@ -7,18 +7,28 @@
 #
 #          Published under GPL-3.0-or-later
 
+# notify-send -u normal -t 5000 "$(
+    # printf "$(date "+%A %-d/%-m/%Y")\n"
+    # printf "$(date "+%H:%M")\n\n"
+    # bat=$(acpi | head -1 | awk {'print $3'})
+    # if [[ $bat == Charging, ]]; then
+        # echo $(acpi | head -1 | awk '{print substr($4, 1, length($4)-1)}'; echo ' charging')
+    # else
+        # echo $(acpi | head -1 | awk '{print substr($4, 1, length($4)-1)}')
+    # fi
+    # printf "$(nmcli -t -f name connection show --active)\n"
+    # printf "$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)"
+    # 
+# )"
+
 notify-send -u normal -t 5000 "$(
+    printf "$(date "+%H:%M")\n"
     printf "$(date "+%A %-d/%-m/%Y")\n"
-    printf "$(date "+%H:%M")\n\n"
     bat=$(acpi | head -1 | awk {'print $3'})
     if [[ $bat == Charging, ]]; then
         echo $(acpi | head -1 | awk '{print substr($4, 1, length($4)-1)}'; echo ' charging')
     else
         echo $(acpi | head -1 | awk '{print substr($4, 1, length($4)-1)}')
     fi
-    printf "$(nmcli -t -f name connection show --active)\n"
-    printf "$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)"
     
 )"
-
-
