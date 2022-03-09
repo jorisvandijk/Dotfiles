9d282be89bf164b74d90f71d2b82f83d93a5ece7
joris <joris@jorisvandijk.com>
2022-03-09 21:10:39 +0100

    Changing directory and adding bookmark

--- bin/bin/ScreenShot.sh
+++ bin/bin/ScreenShot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scrot ~/pictures/ss/%Y-%m-%d-%T-screenshot.png -e 'xclip -selection clipboard -t image/png -i $f'
+scrot ~/Pictures/ss/%Y-%m-%d-%T-screenshot.png -e 'xclip -selection clipboard -t image/png -i $f'
